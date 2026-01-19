221f5ef10fd1d106fd626a70eab3db33cf4fc6c5
Rodrigo <rodrigo@borjaille.com.br>
2026-01-19 18:33:31 -0300

    Create odoo-install.sh

---
+++ install/odoo-install.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+source <(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/build.func)
+# Copyright (c) 2021-2026 community-scripts ORG
+# Author: MickLesk (CanbiZ)
+# License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
+# Source: https://github.com/odoo/odoo
+
+APP="Odoo"
+var_tags="${var_tags:-erp}"
+var_disk="${var_disk:-6}"
+var_cpu="${var_cpu:-2}"
+var_ram="${var_ram:-2048}"
+var_os="${var_os:-debian}"
+var_version="${var_version:-12}"
+var_unprivileged="${var_unprivileged:-1}"
+
+header_info "$APP"
+variables
+color
+catch_errors
+
+function update_script() {
+  header_info
+  check_container_storage
+  check_container_resources
+
+  if [[ ! -f /etc/odoo/odoo.conf ]]; then
+    msg_error "No ${APP} Installation Found!"
+    exit
+  fi
+  if ! [[ $(dpkg -s python3-lxml-html-clean 2>/dev/null) ]]; then
+    $STD apt install python3-lxml
+    curl -fsSL "http://archive.ubuntu.com/ubuntu/pool/universe/l/lxml-html-clean/python3-lxml-html-clean_0.1.1-1_all.deb" -o /opt/python3-lxml-html-clean.deb
+    $STD dpkg -i /opt/python3-lxml-html-clean.deb
+    rm -f /opt/python3-lxml-html-clean.deb
+  fi
+
+  RELEASE="18.0"
+  LATEST_VERSION=$(curl -fsSL "https://nightly.odoo.com/18.0/nightly/deb/odoo_18.0.20240927_all.deb")
+  curl -fsSL https://nightly.odoo.com/18.0/nightly/deb/odoo_18.0.20240927_all.deb -o /opt/odoo.deb
+  $STD apt install -y /opt/odoo.deb
+  rm -f /opt/odoo.deb
+  echo "$LATEST_VERSION" >/opt/${APP}_version.txt
+
+  msg_info "Starting Service"
+  systemctl start odoo
+  msg_ok "Started Service"
+
+start
+build_container
+description
+
+msg_ok "Completed successfully!\n"
+echo -e "${CREATING}${GN}${APP} setup has been successfully initialized!${CL}"
+echo -e "${INFO}${YW} Access it using the following URL:${CL}"
+echo -e "${TAB}${GATEWAY}${BGN}http://${IP}:8069${CL}"
